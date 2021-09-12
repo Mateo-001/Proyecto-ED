fa47913b157690c8dfb3bc1493f29a76140a2802
Mateo <promateo2001@gmail.com>
2021-09-12 16:37:17 -0500

    Primer commit

---
+++ src/MusicPlayer.java
@@ -0,0 +1,54 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+public class MusicPlayer {
+
+	Player player;
+	
+	public MusicPlayer(BufferedInputStream bis) {
+		try {
+			player = new Player(bis);
+		} catch (JavaLayerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void play() {
+				Interfaz.pausa = false;
+		new Thread() {
+	         public void run() {
+	            try {
+	               while (true) {
+	                  if (!Interfaz.pausa) {
+	                     if (!player.play(1)) {
+	                        break;
+	                     }
+	                  }
+	               }
+	            } catch (JavaLayerException e) {
+	               e.printStackTrace();
+	            }
+	         }
+	      }.start();
+	      try {
+	      //Thread.sleep(1);
+	      //Interfaz.pausa = true;
+	      //System.out.println("pausado");
+	      //Thread.sleep(2000);
+	      //Interfaz.pausa = false;
+	      }catch (Exception e) {
+	    	  e.printStackTrace();
+	      }
+	}
+	
+	public void pause() {
+		Interfaz.pausa = true;
+	}
+	
+}

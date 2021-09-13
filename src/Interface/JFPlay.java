de26baeef1d06af29f8897a21d73733b62cfd401
Mateo <promateo2001@gmail.com>
2021-09-13 00:18:37 -0500

    Primer commit

--- src/Interface/JFPlay.java
+++ src/Interface/JFPlay.java
@@ -24,6 +24,7 @@ public class JFPlay extends javax.swing.JFrame {
         this.jTFArtista.setEnabled(false);
         this.jTFCancion.setEnabled(false);
         this.jTFGenero.setEnabled(false);
+    
     }
 
     /**

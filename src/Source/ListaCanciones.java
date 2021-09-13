a7a56226462350fcc95a302afdca8d1c8c09ab7f
Mateo <promateo2001@gmail.com>
2021-09-12 21:17:31 -0500

    Primer commit

--- src/Source/ListaCanciones.java
+++ src/Source/ListaCanciones.java
@@ -1,10 +1,58 @@
 package Source;
 
-
-/**
- *
- * @author Bryan
- */
 public class ListaCanciones {
-    
+
+    public Nodo head;
+
+    public ListaCanciones() {
+        head = new Nodo();
+        head.next = head;
+        head.previous = head;
+    }
+
+    public void insertSong(Song song) {
+        if (head.isEmpty()) {
+            head.song = song;
+        } else {
+            Nodo nuevo = new Nodo(song);
+            Nodo ultimo = head;
+            while (ultimo.next != head) {
+                ultimo = ultimo.next;
+            }
+            nuevo.next = head;
+            ultimo.next = nuevo;
+            head.previous = nuevo;
+            nuevo.previous = ultimo;
+        }
+    }
+
+    public void deleteSong(int index) {
+        Nodo recorre = head;
+        if (index == 1) {
+            head.next.previous = head.previous;
+            head.previous.next = head.next;
+            head = head.next;
+        } else if (index == listSize()) {
+            head.previous.previous.next = head;
+            head.previous = head.previous.previous;
+        }else{
+            for(int i=1;i<index;i++){
+                recorre = recorre.next;
+            }
+            recorre.previous.next = recorre.next;
+            recorre.next.previous = recorre.previous;
+        }
+    }
+
+    public int listSize() {
+        Nodo recorre = head;
+        int i = 0;
+        while (recorre.next != head) {
+            i++;
+            recorre = recorre.next;
+        }
+        i++;
+        return i;
+    }
+
 }
--- src/Source/Nodo.java
+++ src/Source/Nodo.java
@@ -1,10 +1,23 @@
 package Source;
 
-
-/**
- *
- * @author Bryan
- */
 public class Nodo {
+
+    public Song song;
+    public Nodo next;
+    public Nodo previous;
     
+
+    public Nodo() {
+    }
+
+    public Nodo(Song song) {
+        this.song = song;
+        this.next = null;
+        this.previous = null;
+    }
+    
+    public boolean isEmpty(){
+        return song == null;
+    }
+
 }
